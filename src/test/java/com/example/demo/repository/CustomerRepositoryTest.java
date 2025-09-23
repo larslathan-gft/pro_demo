4cb72df3f02e321676463048569d800c43ab6f00
Lars Lathan <65671436+larslathan-gft@users.noreply.github.com>
2025-09-23 19:23:13 +0200

    [GFTCodeFixer]: Update on src/test/java/com/example/demo/repository/CustomerRepositoryTest.java

--- src/test/java/com/example/demo/repository/CustomerRepositoryTest.java
+++ src/test/java/com/example/demo/repository/CustomerRepositoryTest.java
@@ -7,18 +7,18 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-public class CustomerRepositoryTest {
+class CustomerRepositoryTest {
 
     private CustomerRepository customerRepository;
 
     @BeforeEach
-    public void setUp() {
+    void setUp() {
         customerRepository = new CustomerRepository();
         customerRepository.init(); // Para cargar el CSV en memoria
     }
 
     @Test
-    public void testFindAll() {
+    void testFindAll() {
         List<Customer> allCustomers = customerRepository.findAll();
         // Asumimos que en nuestro CSV hay 3 clientes
         Assertions.assertNotNull(allCustomers);
@@ -26,7 +26,7 @@ public class CustomerRepositoryTest {
     }
 
     @Test
-    public void testFindById_Existing() {
+    void testFindById_Existing() {
         Customer customer = customerRepository.findById(1L);
         Assertions.assertNotNull(customer);
         Assertions.assertEquals("Juan", customer.getFirstName());
@@ -34,7 +34,7 @@ public class CustomerRepositoryTest {
     }
 
     @Test
-    public void testFindById_NotExisting() {
+    void testFindById_NotExisting() {
         Customer customer = customerRepository.findById(999L);
         Assertions.assertNull(customer);
     }
