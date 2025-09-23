30bf34bb736bb24453fa7b2b3bd876226032c3ab
Lars Lathan <65671436+larslathan-gft@users.noreply.github.com>
2025-09-23 19:23:08 +0200

    [GFTCodeFixer]: Update on src/test/java/com/example/demo/controller/CustomerControllerTest.java

--- src/test/java/com/example/demo/controller/CustomerControllerTest.java
+++ src/test/java/com/example/demo/controller/CustomerControllerTest.java
@@ -16,7 +16,7 @@ import com.example.demo.service.CustomerService;
 import java.util.Arrays;
 
 @WebMvcTest(CustomerController.class)
-public class CustomerControllerTest {
+class CustomerControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -25,7 +25,7 @@ public class CustomerControllerTest {
     private CustomerService customerService;
 
     @Test
-    public void testGetAllCustomers() throws Exception {
+    void testGetAllCustomers() throws Exception {
         // Mockeamos la respuesta del servicio
         given(customerService.getAllCustomers()).willReturn(
                 Arrays.asList(
@@ -44,7 +44,7 @@ public class CustomerControllerTest {
     }
 
     @Test
-    public void testGetCustomerDetail_Found() throws Exception {
+    void testGetCustomerDetail_Found() throws Exception {
         Customer mockCustomer = new Customer(10L, "Carlos", "López", "carlos@test.com", "555-9012", "789 Pine St");
         given(customerService.getCustomerById(10L)).willReturn(mockCustomer);
 
@@ -59,7 +59,7 @@ public class CustomerControllerTest {
     }
 
     @Test
-    public void testGetCustomerDetail_NotFound() throws Exception {
+    void testGetCustomerDetail_NotFound() throws Exception {
         // Si no se encuentra el cliente, devolvemos null
         given(customerService.getCustomerById(anyLong())).willReturn(null);
 
